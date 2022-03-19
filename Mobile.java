336e2e5988f466a734627bab5cf59a68be035f48
SandhiyaMahe05 <sandhiyasandeepapun1999@gmail.com>
2022-03-19 12:56:01 +0530

    Updated my code

---
+++ Mobile.java
@@ -0,0 +1,18 @@
+package tamilnadu.chennai;
+
+public class Mobile//parent / super class/ Base class
+{
+	
+    public void call() //method definition
+   {
+	    System.out.println("calling");
+   }
+	public void send_message()
+	{
+		System.out.println("Sending message");
+	}
+	public void receive()
+	{
+		System.out.println("Receiving");
+	}
+}
